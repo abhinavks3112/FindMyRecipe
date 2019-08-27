eb04a764f46bb55d2933f2fca970ed14737e065a
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-28 00:32:49 +0530

    Grid setup using FlatList, dummy data with a class

---
+++ models/category.js
@@ -0,0 +1,10 @@
+/* A simple javascript class for defining structure of our grid */
+class Category {
+    constructor(id, title, color) {
+        this.id = id;
+        this.title = title;
+        this.color = color;
+    }
+}
+
+export default Category;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,31 +3,71 @@ import {
     View,
     Text,
     StyleSheet,
-    Button
+    Button,
+    FlatList
 } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+    /* Item data received in renderGridItem is
+    Object {
+        "index": 9,
+        "item": Category {
+            "color": "#47fced",
+            "id": "c10",
+            "title": "Summer",
+        },
+        "separators": Object {
+            "highlight": [Function highlight],
+            "unhighlight": [Function unhighlight],
+            "updateProps": [Function updateProps],
+        },
+    } */
+    return (
+    <View style={styles.gridItem}>
+        <Text>
+            {itemData.item.title}
+        </Text>
+    </View>
+    );
+};
 
 const CategoriesScreen = (props) => {
  buttonPressHandler = () => (
+
+    props.navigation.navigate({ routeName: 'CategoryMeals' })
+    /* Or props.navigation.navigate('CategoryMeals') */
+
     /* Push can take us to screens where navigation won't eg
     we cannot to same screen to same screen, it won't be added to stack
     but if we use push then we can, it will add it to the stack.
     Alo, we don't pass ({routeName: 'Categories'}), but just ('Categories')  */
     // props.navigation.push('Categories')
-    // props.navigation.navigate({ routeName: 'CategoryMeals' })
+
     /* Replace replaces the current component with new component without adding it
     to the cureent stack so now we can't come back to the original screen that has
     been replaced, now the original screen is the one in the current stack
-    eg. we may want to replace 'Login Screen' with home screen after login and 
+    eg. we may want to replace 'Login Screen' with home screen after login and
     we don't want to come back to the login screen using back button or anything */
-    props.navigation.replace({ routeName: 'CategoryMeals' })
-    /* Or props.navigation.navigate('CategoryMeals') */
+    // props.navigation.replace({ routeName: 'CategoryMeals' })
  );
 
  return (
-     <View style={styles.screen}>
-         <Text>Categories Screen</Text>
-         <Button title="Go To Meals" onPress={buttonPressHandler} />
-     </View>
+    /* Item received in KeyExtractor is:
+        Category {
+          "color": "#f5a442",
+          "id": "c3",
+          "title": "Hamburgers",
+        }
+        in newer version, if 'id' is present as key then no need to
+        specify keyExtractor
+    */
+    <FlatList
+    numColumns={2}
+    data={CATEGORIES}
+    renderItem={renderGridItem}
+    keyExtractor={(item, index) => item.id}
+    />
  );
 };
 
@@ -36,6 +76,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
