02f91e6493a3b72da9a1b11d82af773daeaef11e
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-28 12:45:30 +0530

    Recieved Params using getParam

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -5,15 +5,21 @@ import {
  StyleSheet,
  Button
 } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealScreen = (props) => {
- buttonPressHandler = () => {
+ /* We will use getParam to extract params passed from previous screen
+ by passing the same key as in previous page */
+ const catId = props.navigation.getParam('categoryId');
+ const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+ const buttonPressHandler = () => {
      props.navigation.navigate('MealDetail');
  };
  return (
      <View style={styles.screen}>
          <Text>CategoryMeal Screen</Text>
-         <Button title="Go to Category Details Screen" onPress={buttonPressHandler} />        
+         <Text>{selectedCategory.title}</Text>
+         <Button title="Go to Category Details Screen" onPress={buttonPressHandler} />
      </View>
  );
 };
