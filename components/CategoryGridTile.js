711546f85a13f915f38f10f43458dbf4f7b6e474
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-29 15:53:06 +0530

    Fixed Shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -36,7 +36,12 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: (
+                    Platform.OS === 'android' && Platform.Version >= 21
+                    ? 'hidden'
+                    : 'visible'
+                  ),
+        elevation: 5
     },
     tile: {
         flex: 1,
@@ -46,8 +51,7 @@ const styles = StyleSheet.create({
         shadowOffset: {
             width: 0,
             height: 2
-        },
-        elevation: 3,
+        },       
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
