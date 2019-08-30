2be4320c891031716d781e62837ffad8749c0348
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-30 14:12:16 +0530

    Added content and styling to MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -3,28 +3,43 @@ import {
     View,
     Text,
     StyleSheet,
-    Button
+    ScrollView,
+    Image
  } from 'react-native';
  import { HeaderButtons, Item } from 'react-navigation-header-buttons';
  import { MEALS } from '../data/dummy-data';
  import HeaderButton from '../components/HeaderButton';
+ import DefaultText from '../components/DefaultText';
 
+const ListItem = (props) => (
+    <View style={styles.listItem}>
+        <DefaultText>{props.children}</DefaultText>
+    </View>
+);
 
 const MealDetailScreen = (props) => {
     const { navigation } = props;
     const mealId = navigation.getParam('mealId');
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
  return (
-     <View style={styles.screen}>
-         <Text>{selectedMeal.title}</Text>
-         {/* Difference between 'goBack' and 'pop'(small letter) is
-         that goBack can also be used in other types of navigation,
-         while 'pop' only works with Stack Navigator */}
-         <Button title="Go Back" onPress={() => props.navigation.goBack()} />
-         <Button title="Pop this screen off" onPress={() => props.navigation.pop()} />
-          {/* Go back to root of stack */}
-         <Button title="Home" onPress={() => props.navigation.popToTop()} />
-     </View>
+     <ScrollView>
+        <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+        <View style={styles.details}>
+                        <DefaultText>
+                        {selectedMeal.duration}
+                        m
+                        </DefaultText>
+                        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+        </View>
+        <Text style={styles.title}>Ingredients</Text>
+        {selectedMeal.ingredients.map((ingredient) => (
+            <ListItem key={ingredient}>{ingredient}</ListItem>))}
+        <Text style={styles.title}>Steps</Text>
+        {selectedMeal.steps.map((step) => (
+            <ListItem key={step}>{step}</ListItem>))}
+     </ScrollView>
  );
 };
 
@@ -47,10 +62,27 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        marginVertical: 10,
+        fontSize: 22,
+        fontFamily: 'open-sans-bold',
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10
     }
 });
 
